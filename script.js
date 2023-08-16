27b3b5702d725a2375ca9fac354d293a5ebbd983
Felipe Schiavinatto Costa <schiavinatto.felipe@gmail.com>
2023-08-16 15:18:15 -0300

    foi colocado mais funcionalidades no código javascript, principalmente no que se refere à manipulação do DOM

--- script.js
+++ script.js
@@ -28,19 +28,66 @@ let email_outorgado;
 
 let endereco_outorgado;
 
-//-------variaveis que conterão os botões-----
+//-------variaveis que conterão os botões e elementos escondidos-----
 
-let botao_ir_outorgado;
+let botao_ir_outorgado = document.getElementById("ir_outorgado");
 
-let botao_gerarProcuracao = document.getElementById("gerarProcuracao")
+let botao_gerarProcuracao = document.getElementById("gerarProcuracao");
+
+let criarNova = document.getElementById("criarNova");
+
+let box_outorgado = document.getElementById("box_outorgado");
+
+let box_outorgante = document.getElementById("box_outorgante");
+
+let secaoResultado = document.getElementById("secaoResultado");
+
+let meuForm = document.getElementById("meuForm");
 
 //---------------------
+
+botao_ir_outorgado.addEventListener('click', irOutorgado);
+
+function irOutorgado(){
+
+   nomeOutorgante = document.getElementById("nomeOutorgante").value;
+    nomeOutorgante = nomeOutorgante.toUpperCase();
+
+    nacionalidadeOutorgante = document.getElementById("nacionalidadeOutorgante").value;
+    
+    estadoCivil_outorgante = document.getElementById("estadoCivil_outorgante").value;
+    
+    profissao_outorgante = document.getElementById("profissao_outorgante").value;
+    
+    cpf_outorgante = document.getElementById("cpf_outorgante").value;
+    
+    email_outorgante = document.getElementById("email_outorgante").value;
+    
+    endereco_outorgante = document.getElementById("endereco_outorgante").value;
+     
+    if(nomeOutorgante=="" || nacionalidadeOutorgante=="" || estadoCivil_outorgante==="selecione" || profissao_outorgante==""
+        || cpf_outorgante=="" || email_outorgante==""|| endereco_outorgante==""  ){
+            alert("Um ou mais campos não foram preenchidos. Verifique e tente novamente!")
+        }
+    else{
+        box_outorgante.style.display = 'none'
+        box_outorgado.style.display = 'block' 
+    }     
+}
+
+
+
+
+
+
+//-----------------------------------
 
 botao_gerarProcuracao.addEventListener('click', gerarProcuracao);
 
 function gerarProcuracao(){
 
     nomeOutorgante = document.getElementById("nomeOutorgante").value;
+    nomeOutorgante = nomeOutorgante.toUpperCase();
 
     nacionalidadeOutorgante = document.getElementById("nacionalidadeOutorgante").value;
     
@@ -56,6 +103,7 @@ function gerarProcuracao(){
     //-------------------------------------
 
     nomeOutorgado = document.getElementById("nomeOutorgado").value;
+    nomeOutorgado = nomeOutorgado.toUpperCase();
 
     nacionalidadeOutorgado = document.getElementById("nacionalidadeOutorgado").value;
 
@@ -68,6 +116,23 @@ function gerarProcuracao(){
     endereco_outorgado = document.getElementById("endereco_outorgado").value;
 
     //---------------------------------------------
+
+    let assinatura_outorgante = document.getElementById("assinatura_outorgante");
+    let cpf_assinatura_outorgante = document.getElementById("cpf_assinatura_outorgante");
+    //-------------------------------------
+
+    if(nomeOutorgado=="" || nacionalidadeOutorgado=="" || estadoCivil_outorgado=="selecione"
+    || oab_outorgado=="" || email_outorgado==""|| endereco_outorgado==""  ){
+        alert("Um ou mais campos não foram preenchidos. Verifique e tente novamente!")
+    }
+    else{
+        box_outorgado.style.display = 'none'
+        secaoResultado.style.display = 'block'
+    }
+
+
+
+    //------------------------------------------------
 
     let campoNome_outorgante = document.getElementById("campoNome_outorgante");
     campoNome_outorgante.innerHTML = nomeOutorgante;
@@ -106,9 +171,21 @@ function gerarProcuracao(){
 
     let campoEndereço_outorgado = document.getElementById("campoEndereço_outorgado");
     campoEndereço_outorgado.innerHTML = endereco_outorgado;
+    //------------------------------------------
+    assinatura_outorgante.innerHTML = nomeOutorgante;
+    cpf_assinatura_outorgante.innerHTML = cpf_outorgante;
 
 
 }
+
+
+criarNova.addEventListener('click', resetar);
+
+function resetar(){
+    meuForm.reset();
+    secaoResultado.style.display = 'none';
+    box_outorgante.style.display = 'block'
+  }
 
 
 
