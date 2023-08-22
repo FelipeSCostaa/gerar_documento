c356f9332b217d00be8434fe5fda022f0d873d35
Felipe Schiavinatto Costa <schiavinatto.felipe@gmail.com>
2023-08-21 22:09:28 -0300

    finalizando estilização da página

--- script.js
+++ script.js
@@ -88,95 +88,95 @@ botao_gerarProcuracao.addEventListener('click', gerarProcuracao);
 
 function gerarProcuracao(){
 
-    nomeOutorgante = document.getElementById("nomeOutorgante").value;
-    nomeOutorgante = nomeOutorgante.toUpperCase();
+            nomeOutorgante = document.getElementById("nomeOutorgante").value;
+            nomeOutorgante = nomeOutorgante.toUpperCase();
 
-    nacionalidadeOutorgante = document.getElementById("nacionalidadeOutorgante").value;
-    
-    estadoCivil_outorgante = document.getElementById("estadoCivil_outorgante").value;
-    
-    profissao_outorgante = document.getElementById("profissao_outorgante").value;
-    
-    cpf_outorgante = document.getElementById("cpf_outorgante").value;
-    
-    email_outorgante = document.getElementById("email_outorgante").value;
-    
-    endereco_outorgante = document.getElementById("endereco_outorgante").value;
-    //-------------------------------------
+            nacionalidadeOutorgante = document.getElementById("nacionalidadeOutorgante").value;
+            
+            estadoCivil_outorgante = document.getElementById("estadoCivil_outorgante").value;
+            
+            profissao_outorgante = document.getElementById("profissao_outorgante").value;
+            
+            cpf_outorgante = document.getElementById("cpf_outorgante").value;
+            
+            email_outorgante = document.getElementById("email_outorgante").value;
+            
+            endereco_outorgante = document.getElementById("endereco_outorgante").value;
+            //-------------------------------------
 
-    nomeOutorgado = document.getElementById("nomeOutorgado").value;
-    nomeOutorgado = nomeOutorgado.toUpperCase();
+            nomeOutorgado = document.getElementById("nomeOutorgado").value;
+            nomeOutorgado = nomeOutorgado.toUpperCase();
 
-    nacionalidadeOutorgado = document.getElementById("nacionalidadeOutorgado").value;
+            nacionalidadeOutorgado = document.getElementById("nacionalidadeOutorgado").value;
 
-    estadoCivil_outorgado = document.getElementById("estadoCivil_outorgado").value;
+            estadoCivil_outorgado = document.getElementById("estadoCivil_outorgado").value;
 
-    oab_outorgado = document.getElementById("oab_outorgado").value;
+            oab_outorgado = document.getElementById("oab_outorgado").value;
 
-    email_outorgado = document.getElementById("email_outorgado").value;
+            email_outorgado = document.getElementById("email_outorgado").value;
 
-    endereco_outorgado = document.getElementById("endereco_outorgado").value;
+            endereco_outorgado = document.getElementById("endereco_outorgado").value;
 
-    //---------------------------------------------
+            //---------------------------------------------
 
-    let assinatura_outorgante = document.getElementById("assinatura_outorgante");
-    let cpf_assinatura_outorgante = document.getElementById("cpf_assinatura_outorgante");
-    //-------------------------------------
+            let assinatura_outorgante = document.getElementById("assinatura_outorgante");
+            let cpf_assinatura_outorgante = document.getElementById("cpf_assinatura_outorgante");
+            //-------------------------------------
 
-    if(nomeOutorgado=="" || nacionalidadeOutorgado=="" || estadoCivil_outorgado=="selecione"
-    || oab_outorgado=="" || email_outorgado==""|| endereco_outorgado==""  ){
-        alert("Um ou mais campos não foram preenchidos. Verifique e tente novamente!")
-    }
-    else{
-        box_outorgado.style.display = 'none'
-        secaoResultado.style.display = 'flex'
-        titulo_principal.style.display = 'none'
-    }
+            if(nomeOutorgado=="" || nacionalidadeOutorgado=="" || estadoCivil_outorgado=="selecione"
+            || oab_outorgado=="" || email_outorgado==""|| endereco_outorgado==""  ){
+                alert("Um ou mais campos não foram preenchidos. Verifique e tente novamente!")
+            }
+            else{
+                box_outorgado.style.display = 'none'
+                secaoResultado.style.display = 'flex'
+                titulo_principal.style.display = 'none'
+            }
 
 
 
-    //------------------------------------------------
+            //------------------------------------------------
 
-    let campoNome_outorgante = document.getElementById("campoNome_outorgante");
-    campoNome_outorgante.innerHTML = nomeOutorgante;
+            let campoNome_outorgante = document.getElementById("campoNome_outorgante");
+            campoNome_outorgante.innerHTML = nomeOutorgante;
 
-    let campoNacionalidade_outorgante = document.getElementById("campoNacionalidade_outorgante");
-    campoNacionalidade_outorgante.innerHTML = nacionalidadeOutorgante;
+            let campoNacionalidade_outorgante = document.getElementById("campoNacionalidade_outorgante");
+            campoNacionalidade_outorgante.innerHTML = nacionalidadeOutorgante;
 
-    let campoEstatusCivil_outorgante = document.getElementById("campoEstatusCivil_outorgante");
-    campoEstatusCivil_outorgante.innerHTML = estadoCivil_outorgante;
+            let campoEstatusCivil_outorgante = document.getElementById("campoEstatusCivil_outorgante");
+            campoEstatusCivil_outorgante.innerHTML = estadoCivil_outorgante;
 
-    let campoCPF_outorgante = document.getElementById("campoCPF_outorgante");
-    campoCPF_outorgante.innerHTML = cpf_outorgante;
+            let campoCPF_outorgante = document.getElementById("campoCPF_outorgante");
+            campoCPF_outorgante.innerHTML = cpf_outorgante;
 
-    let campoEmail_outorgante = document.getElementById("campoEmail_outorgante");
-    campoEmail_outorgante.innerHTML = email_outorgante;
+            let campoEmail_outorgante = document.getElementById("campoEmail_outorgante");
+            campoEmail_outorgante.innerHTML = email_outorgante;
 
-    let campoEndereço_outorgante = document.getElementById("campoEndereço_outorgante");
-    campoEndereço_outorgante.innerHTML = endereco_outorgante;
+            let campoEndereço_outorgante = document.getElementById("campoEndereço_outorgante");
+            campoEndereço_outorgante.innerHTML = endereco_outorgante;
 
-    //------------------------------------------
+            //------------------------------------------
 
-    let campoNome_outorgado = document.getElementById("campoNome_outorgado");
-    campoNome_outorgado.innerHTML = nomeOutorgado;
+            let campoNome_outorgado = document.getElementById("campoNome_outorgado");
+            campoNome_outorgado.innerHTML = nomeOutorgado;
 
-    let campoNacionalidade_outorgado = document.getElementById("campoNacionalidade_outorgado");
-    campoNacionalidade_outorgado.innerHTML = nacionalidadeOutorgado;
+            let campoNacionalidade_outorgado = document.getElementById("campoNacionalidade_outorgado");
+            campoNacionalidade_outorgado.innerHTML = nacionalidadeOutorgado;
 
-    let campoEstatusCivil_outorgado = document.getElementById("campoEstatusCivil_outorgado");
-    campoEstatusCivil_outorgado.innerHTML = estadoCivil_outorgado;
+            let campoEstatusCivil_outorgado = document.getElementById("campoEstatusCivil_outorgado");
+            campoEstatusCivil_outorgado.innerHTML = estadoCivil_outorgado;
 
-    let campoOAB_outorgado = document.getElementById("campoOAB_outorgado");
-    campoOAB_outorgado.innerHTML = oab_outorgado;
+            let campoOAB_outorgado = document.getElementById("campoOAB_outorgado");
+            campoOAB_outorgado.innerHTML = oab_outorgado;
 
-    let campoEmail_outorgado = document.getElementById("campoEmail_outorgado");
-    campoEmail_outorgado.innerHTML = email_outorgado;
+            let campoEmail_outorgado = document.getElementById("campoEmail_outorgado");
+            campoEmail_outorgado.innerHTML = email_outorgado;
 
-    let campoEndereço_outorgado = document.getElementById("campoEndereço_outorgado");
-    campoEndereço_outorgado.innerHTML = endereco_outorgado;
-    //------------------------------------------
-    assinatura_outorgante.innerHTML = nomeOutorgante;
-    cpf_assinatura_outorgante.innerHTML = cpf_outorgante;
+            let campoEndereço_outorgado = document.getElementById("campoEndereço_outorgado");
+            campoEndereço_outorgado.innerHTML = endereco_outorgado;
+            //------------------------------------------
+            assinatura_outorgante.innerHTML = nomeOutorgante;
+            cpf_assinatura_outorgante.innerHTML = cpf_outorgante;
 
 
 }
@@ -187,7 +187,7 @@ criarNova.addEventListener('click', resetar);
 function resetar(){
     meuForm.reset();
     secaoResultado.style.display = 'none';
-    box_outorgante.style.display = 'block'
+    box_outorgante.style.display = 'flex'
   }
 
 
